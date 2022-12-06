73e2f303aa1e0b5c7e76f693b0aa0df2d2a4f443
srash007 <sarahrashiwa@gmail.com>
2022-12-06 15:23:04 -0500

    -git commit"Update"

--- Mealer - Livrable4/app/src/main/java/com/example/mealer/EvaluationCuisinier.java
+++ Mealer - Livrable4/app/src/main/java/com/example/mealer/EvaluationCuisinier.java
@@ -1,8 +1,11 @@
 package com.example.mealer;
 
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +13,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
 public class EvaluationCuisinier extends AppCompatActivity {
-    ListView list;
+    RadioButton R1, R2, R3, R4, R5;
+    TextView description;
+    Button Retour;
     dbHelper2 DB;
+    String user ;
+    int choix;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluation);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            user = extras.getString("key");
+        }
+
+        DB = new dbHelper2(this);
+        Retour = (Button)findViewById(R.id.retour);
+        R1 = (RadioButton) findViewById(R.id.MauvaisService);
+        R2 = (RadioButton) findViewById(R.id.Passable);
+        R3 = (RadioButton) findViewById(R.id.Correct);
+        R4 = (RadioButton) findViewById(R.id.Bon);
+        R5 = (RadioButton) findViewById(R.id.Excellent);
+        description = (TextView) findViewById(R.id.Explication);
+
+      Retour.setOnClickListener(new View.OnClickListener(){
+
+          @Override
+          public void onClick(View v) {
+              if (R1.callOnClick()){
+                  choix = 1;
+              }
+              if (R2.callOnClick()){
+                  choix = 2;
+              }
+              if (R3.callOnClick()){
+                  choix = 3;
+              }
+              if (R4.callOnClick()){
+                  choix = 4;
+              }
+              if (R5.callOnClick()){
+                  choix = 5;
+              }
+
+          }
+      });
+
+
+    }
 
 }
